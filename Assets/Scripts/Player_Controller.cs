a6c5b32566e48ac6b9f46bd9cbbb9166215c44dd
photofisken <julialuma@hotmail.com>
2018-09-19 21:49:17 +0200

    Wallie movement

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+	void Start () {
+		
+	}
+	
+	void Update ()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical, 0f);
+        movement.Normalize();
+
+        transform.position += movement * speed * Time.deltaTime;
+    }
+}
