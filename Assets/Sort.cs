99031e8f9d1f90b067f5f230ec85bde2f1989a9c
photofisken <julialuma@hotmail.com>
2018-09-19 20:49:37 +0200

    Sprites commit
    lots of sprites

---
+++ Assets/Sort.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sort : MonoBehaviour
+{
+    [SerializeField]
+    private bool dynamic;
+    private SpriteRenderer sr;
+
+	void Start ()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        SetZ();
+    }
+
+    void Update ()
+    {
+        if(dynamic)
+            SetZ();
+	}
+
+    private void SetZ()
+    {
+        transform.position -= new Vector3(0f, 0f, (transform.position.y + sr.bounds.size.y / 2f) / 10f);
+    }
+}
